c3879a7b6e18625e0c10d7e8c32fea1bc311984f
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-12-04 11:19:33 +0700

    Bootstraps added

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
 
-	echo "
-	Ini cerita pendek <br>
-	Ceritanya ga panjang <br>
-	Kalau ceritanya panjang <br>
-	Namanya cerita panjang <br>
-	Bukan cerita pendek <br>
-	";
-
-	echo "<br/>Apaansih?";
-
-	echo "Kereeen";
+	
 ?>
