3e8c163307b8f16fc284546125de3d0c29ca2b21
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-12-09 15:58:48 +0700

    sql klinik yang baru

---
+++ assets/database/klinik.sql
@@ -0,0 +1,360 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 09 Des 2017 pada 09.58
+-- Versi Server: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `klinik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `password` text NOT NULL,
+  `nama` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `antrian`
+--
+
+CREATE TABLE `antrian` (
+  `id_antrian` int(8) NOT NULL,
+  `no_rekam_medis` varchar(12) NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` text NOT NULL,
+  `jam_daftar` time NOT NULL,
+  `jam_layan` time NOT NULL,
+  `no_reg_dokter` varchar(12) NOT NULL,
+  `status_pasien` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `booking`
+--
+
+CREATE TABLE `booking` (
+  `id_booking` int(8) NOT NULL,
+  `no_rekam_medis` varchar(12) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `nama_pasien` text NOT NULL,
+  `no_telp` text NOT NULL,
+  `no_reg_dokter` varchar(12) NOT NULL,
+  `status_pasien` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_jasa` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `jml_obat_per_satuan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id_dokter` int(11) NOT NULL,
+  `no_reg_dokter` varchar(12) NOT NULL,
+  `nama_dokter` text NOT NULL,
+  `alamat` text NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `password` text NOT NULL,
+  `no_telp` text NOT NULL,
+  `email` text NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jasa`
+--
+
+CREATE TABLE `jasa` (
+  `id_jasa` int(8) NOT NULL,
+  `nama_jasa` text NOT NULL,
+  `tarif` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(8) NOT NULL,
+  `nama_obat` text NOT NULL,
+  `satuan` text NOT NULL,
+  `ukuran` text NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id_pasien` int(11) NOT NULL,
+  `nama_pasien` text NOT NULL,
+  `alamat` text NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `pekerjaan` text NOT NULL,
+  `no_telp` text NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `no_rekam_medis` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `perawat`
+--
+
+CREATE TABLE `perawat` (
+  `id_perawat` int(11) NOT NULL,
+  `no_reg_perawat` varchar(12) NOT NULL,
+  `nama_perawat` text NOT NULL,
+  `alamat` text NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `password` text NOT NULL,
+  `no_telp` text NOT NULL,
+  `email` text NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `no_rekam_medis` varchar(12) NOT NULL,
+  `no_reg_dokter` varchar(12) NOT NULL,
+  `no_reg_perawat` varchar(12) NOT NULL,
+  `tanggal` date NOT NULL,
+  `diagnosa` text NOT NULL,
+  `terapi` text NOT NULL,
+  `keterangan` text NOT NULL,
+  `jam` time NOT NULL,
+  `jenis_pembayaran` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `antrian`
+--
+ALTER TABLE `antrian`
+  ADD PRIMARY KEY (`id_antrian`),
+  ADD UNIQUE KEY `no_rekam_medis` (`no_rekam_medis`),
+  ADD KEY `no_reg_dokter` (`no_reg_dokter`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id_booking`),
+  ADD UNIQUE KEY `no_rekam_medis` (`no_rekam_medis`),
+  ADD KEY `no_reg_dokter` (`no_reg_dokter`);
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_jasa` (`id_jasa`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id_dokter`),
+  ADD UNIQUE KEY `no_reg` (`no_reg_dokter`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `jasa`
+--
+ALTER TABLE `jasa`
+  ADD PRIMARY KEY (`id_jasa`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id_pasien`),
+  ADD UNIQUE KEY `no_rekam_medis` (`no_rekam_medis`);
+
+--
+-- Indexes for table `perawat`
+--
+ALTER TABLE `perawat`
+  ADD PRIMARY KEY (`id_perawat`),
+  ADD UNIQUE KEY `no_reg_perawat` (`no_reg_perawat`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD UNIQUE KEY `no_rekam_medi` (`no_rekam_medis`),
+  ADD KEY `no_reg_dokter` (`no_reg_dokter`),
+  ADD KEY `no_reg_perawat` (`no_reg_perawat`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `antrian`
+--
+ALTER TABLE `antrian`
+  MODIFY `id_antrian` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id_booking` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jasa`
+--
+ALTER TABLE `jasa`
+  MODIFY `id_jasa` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `perawat`
+--
+ALTER TABLE `perawat`
+  MODIFY `id_perawat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `antrian`
+--
+ALTER TABLE `antrian`
+  ADD CONSTRAINT `antrian_ibfk_1` FOREIGN KEY (`no_rekam_medis`) REFERENCES `pasien` (`no_rekam_medis`),
+  ADD CONSTRAINT `antrian_ibfk_2` FOREIGN KEY (`no_reg_dokter`) REFERENCES `dokter` (`no_reg_dokter`);
+
+--
+-- Ketidakleluasaan untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`no_rekam_medis`) REFERENCES `pasien` (`no_rekam_medis`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`no_reg_dokter`) REFERENCES `dokter` (`no_reg_dokter`);
+
+--
+-- Ketidakleluasaan untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_jasa`) REFERENCES `jasa` (`id_jasa`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`no_rekam_medis`) REFERENCES `pasien` (`no_rekam_medis`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`no_reg_dokter`) REFERENCES `dokter` (`no_reg_dokter`),
+  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`no_reg_perawat`) REFERENCES `perawat` (`no_reg_perawat`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
