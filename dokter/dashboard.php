1f5af65547f8024ef9ef8f6b3e5441879a8a5378
megabps <34057916+megabps@users.noreply.github.com>
2018-01-06 00:20:34 +0700

    editdashboard

--- dokter/dashboard.php
+++ dokter/dashboard.php
@@ -54,6 +54,7 @@
     <link href="../assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
     <link href="../assets/font-awesome/css/font-awesome.css" rel="stylesheet" >
+    <link href="../assets/icofont/css/icofont.css" rel="stylesheet" >
     <link rel="stylesheet" type="text/css" href="../assets/lineicons/style.css"> 
 
     <!-- Custom styles for this template -->
@@ -74,90 +75,69 @@
     <!--MAIN CONTENT START-->
     <section id="main-content">
         <section class="wrapper">
-         
-            <div class="row mtbox">
-                  <!--BOX DOKTER-->
-                  <a href="dokter.php"> 
-                  <div class="col-md-2 col-sm-2 col-md-offset-1 box0">
-                    <div class="box1">
-                      <span class="li_heart"></span>
-                      <h3>DOKTER</h3>
-                    </div>
-                  <p>Data Dokter Riona Dental Care</p>
-                  </div></a>
+          <?php
+        $sql  = "SELECT * FROM user_klinik, dokter WHERE user_klinik.username = '$_SESSION[uid]' and user_klinik.id_user_klinik=dokter.id_user_klinik";
+        $result = mysqli_query($conn, $sql);
+      ?>
+          <div class="row mt">
+          <div class="col-lg-12">
+            <div class="row">
+            <!--panel antrian-->
 
-                  <!--BOX PERAWAT-->
-                  <a href="perawat.php"> 
-                  <div class="col-md-2 col-sm-2 box0">
-                    <div class="box1">
-                      <span class="li_heart"></span>
-                      <h3>PERAWAT</h3>
+           <?php while($row=mysqli_fetch_assoc($result))
+           echo "
+            <div class=\"col-lg-4 col-md-4 col-sm-4 col-md-offset-4 mb\">
+              <div class=\"white-panel pn\">
+                <div class=\"white-header\">
+                 <h2>PROFILE</h2>
+                </div>
+                <p><img src=\"assets/img/login-bgg.jpg\" class=\"img-circle\" width=\"50\"></p>
+                <p class=\"medium mt\"><b>".$row['nama_dokter']."</b></p>
+                  <div class=\"row\">
+                    <div class=\"col-md-6\">
+                      <p class=\"small mt\">".$row['alamat']."</p>
+                      <p>".$row['no_telp']."</p>
                     </div>
-                  <p>Data Perawat Riona Dental Care</p>
-                  </div></a>
-
-                  <!--BOX PASIEN-->
+                    <div class=\"col-md-6\">
+                      <p class=\"small mt\">".$row['tanggal_lahir']."</p>
+                      <p>".$row['email']."</p>
+                    </div>
+                  </div>
+              </div>
+            </div><!-- /col-md-4 -->
+            </div>
+            ";
+        ?>
+                  <!--BOX PASIEN--><br><br>
                   <a href="pasien.php"> 
-                  <div class="col-md-2 col-sm-2 box0">
+                  <div class="col-md-2 col-sm-2 col-md-offset-3 box0">
                     <div class="box1">
-                      <span class="li_heart"></span>
+                      <span class="fa fa-users"></span>
                       <h3>PASIEN</h3>
                     </div>
                   <p>Data Pasien Riona Dental Care</p>
                   </div></a>
 
-                  <!--BOX OBAT-->
-                  <a href="obat.php"> 
+                  <!--BOX ANTRIAN-->
+                  <a href="antrian.php"> 
                   <div class="col-md-2 col-sm-2 box0">
                     <div class="box1">
-                      <span class="li_heart"></span>
-                      <h3>OBAT</h3>
+                      <span class="fa fa-tasks"></span>
+                      <h3>ANTRIAN</h3>
                     </div>
-                  <p>Daftar Obat Riona Dental Care</p>
+                  <p>Daftar Antrian Pasien Riona Dental Care </p>
                   </div></a>
 
-                  <!--BOX TERAPI-->
+                  <!--BOX BOOKING-->
                   <a href="terapi.php"> 
                   <div class="col-md-2 col-sm-2 box0">
                     <div class="box1">
-                      <span class="li_heart"></span>
-                      <h3>TERAPI</h3>
+                      <span class="fa fa-bell-o"></span>
+                      <h3>BOOKING</h3>
                     </div>
-                  <p>Jenis Terapi Riona Dental Care</p>
+                  <p>Daftar Booking Pasien Riona Dental Care</p>
                   </div></a>
-          </div><!-- /row mt -->
-          <center>
-          <div class="row mt">
-          <div class="col-lg-12">
-            <div class="row">
-            <!--panel antrian-->
-              <div class="col-lg-4 col-md-4 col-sm-4 mb">
-              <div class="content-panel pn">
-                <div id="profile-01">
-                  <h1>ANTRIAN</h1>
-                </div>
-                <div class="profile-01 centered">
-                  <a href="antrian_hari_ini.php"><p>ANTRIAN HARI INI</p></a>
-                </div>
-                <div class="profile-01 centered">
-                  <a href="antrian_semua.php"><p>ANTRIAN KESELURUHAN</p></a>
-                </div>
-              </div><!--/content-panel -->
-            </div><!--/col-md-4 -->
-
-            <a href="booking.php">
-            <div class="col-md-4 col-sm-4 mb">
-              <div class="weather pn">
-                <i class="fa fa-cloud fa-4x"></i>
-                <h1>BOOKING</h1>
-              </div>
-            </div>
-          </a>
-
-         </div>
-       </div>
-     </div></center>
- </section>
+      </section>
     </section>
     <!--MAIN CONTENT END-->
   </section>
